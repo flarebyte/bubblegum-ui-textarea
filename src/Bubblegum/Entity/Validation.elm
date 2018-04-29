9fe301abe77f42d045f7a144de516413bbb96b48
olih <olih@users.noreply.github.com>
2018-04-29 11:49:59 +0100

    wip

--- src/Bubblegum/Entity/Validation.elm
+++ src/Bubblegum/Entity/Validation.elm
@@ -14,6 +14,22 @@ matchListSize: Int -> List String -> Bool
 matchListSize expected list =
       List.length list == expected 
 
+helperMoreThan: Int -> List String -> Bool
+helperMoreThan expected list =
+      List.length list >= expected 
+
+helperStrictlyMoreThan: Int -> List String -> Bool
+helperStrictlyMoreThan expected list =
+      List.length list > expected 
+
+helperLessThan: Int -> List String -> Bool
+helperLessThan expected list =
+      List.length list <= expected 
+
+helperStrictlyLessThan: Int -> List String -> Bool
+helperStrictlyLessThan expected list =
+      List.length list < expected 
+
 onlyOne: List String -> String
 onlyOne list =
     List.head list |> Maybe.withDefault "should-never-happen"
@@ -33,4 +49,24 @@ asSingle outcome =
 asTuple: Outcome (List String) -> Outcome (String, String)
 asTuple outcome =
     Outcome.check (matchListSize 2) "unsatisfied-constraint:tuple" outcome |> Outcome.map onlyTuple
+
+asListEqual: Int -> Outcome (List String) -> Outcome (List String)
+asListEqual size outcome =
+    Outcome.check (matchListSize size) ("unsatisfied-constraint:list-equal-" ++ (toString size)) outcome
+
+asListMoreThan: Int -> Outcome (List String) -> Outcome (List String)
+asListMoreThan size outcome =
+    Outcome.check (helperMoreThan size) ("unsatisfied-constraint:list-more-than-" ++ (toString size)) outcome
+
+asListStrictlyMoreThan: Int -> Outcome (List String) -> Outcome (List String)
+asListStrictlyMoreThan size outcome =
+    Outcome.check (helperStrictlyMoreThan size) ("unsatisfied-constraint:list-strictly-more-than-" ++ (toString size)) outcome
+
+asListLessThan: Int -> Outcome (List String) -> Outcome (List String)
+asListLessThan size outcome =
+    Outcome.check (helperLessThan size) ("unsatisfied-constraint:list-less-than-" ++ (toString size)) outcome
+
+asListStrictlyLessThan: Int -> Outcome (List String) -> Outcome (List String)
+asListStrictlyLessThan size outcome =
+    Outcome.check (helperStrictlyLessThan size) ("unsatisfied-constraint:list-strictly-less-than-" ++ (toString size)) outcome
 
