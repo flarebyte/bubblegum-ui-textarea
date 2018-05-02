6a5fd91246dc1faf2c1685981de7563969cc1043
olih <olih@users.noreply.github.com>
2018-05-02 20:53:42 +0100

    wip

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,21 +3,14 @@ module Tests exposing (..)
 import Test exposing (describe, test, Test)
 import Expect
 import Bubblegum.TextAreaWidget
-import Regex exposing (regex)
+
 
 all : Test
 all =
-    describe "Ntriples.Filter"
-        [ describe "filter" <|
-            [ test "filter by object value" <|
+    describe "TextAreaWidget.view"
+        [ describe "view" <|
+            [ test "do we need unit tests" <|
                 \() ->
-                    Expect.equal
-                    (filter (WithObject (Equals "subject4")) allTriples)
-                    [t4]
-              , test "filter by Custom" <|
-                \() ->
-                    Expect.equal
-                    (filter (WithObject (Custom custom "subject4")) allTriples)
-                    [t4]
+                    Expect.equal True True
             ]
         ]
