bdca5431db99f5c7a1a57770d4c8c14d30341e52
olih <olih@users.noreply.github.com>
2018-05-04 23:51:34 +0100

    wip

--- src/Bubblegum/TextAreaWidget.elm
+++ src/Bubblegum/TextAreaWidget.elm
@@ -20,9 +20,17 @@ import List
 import String exposing (lines, words)
 
 
+{-| rows = number of lines + lines of with an average 80 chars
+-}
 calculateRows : String -> String
 calculateRows content =
-    content |> lines |> List.length |> (+) 1 |> toString
+    let
+        carriageReturns = content |> lines |> List.length
+        numberOfChars = content |> String.length
+        numberOfAvgLines = numberOfChars // 80
+        numberOfLines = carriageReturns + numberOfAvgLines + 1
+    in
+        toString (if numberOfLines < 40 then numberOfLines else 40)
 
 
 {-| The core representation of a field.
@@ -36,14 +44,11 @@ view adapter userSettings settings state =
         addValue =
             appendAttributeIfSuccess value (getContent state)
 
-        addValueInDiv =
-            appendIfSuccess (\v -> div [] [ text v ]) (getContent state)
-
         addRows =
             appendAttributeIfSuccess (attribute "rows") (getContent state |> Outcome.map calculateRows)
     in
     div [ class "box is-marginless is-paddingless is-shadowless has-addons" ]
-        ([ textarea
+        [ textarea
             ([ class "textarea is-marginless is-paddingless is-shadowless"
              , onInput adapter.onInput
              ]
@@ -52,7 +57,5 @@ view adapter userSettings settings state =
                 |> addRows
             )
             []
-         , displayTextInfo adapter userSettings settings state
-         ]
-            |> addValueInDiv
-        )
+        , displayTextInfo adapter userSettings settings state
+        ]
