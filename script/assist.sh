eb56214cff7c361ffde653974f8f7d25a323321d
Olivier Huin <olih@users.noreply.github.com>
2022-07-12 11:15:02 +0100

    Generate generator script

--- script/assist.sh
+++ script/assist.sh
@@ -1,3 +1,1 @@
 #!/bin/sh
-echo "Generating assistance for Vocabulary ..."
-# npx baldrick-whisker@latest render --diff generated/vocabulary.json script/template/test-data.hbs tests/VocabularyTestData.elm
--- script/generate.sh
+++ script/generate.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 npx baldrick-whisker@latest render elm.json script/template/widget-package.hbs demo/WidgetPackageJson.elm
-echo "Generating Vocabulary ..."
-# npx baldrick-whisker@latest object generated/vocabulary.json src/Bubblegum/TextArea/Vocabulary.elm script/data/module/vocabulary.json
-# npx ajv validate --errors text -s script/schema/merged.schema.json -d generated/vocabulary.json
-# npx baldrick-whisker@latest render generated/vocabulary.json script/template/tests.hbs tests/VocabularyTests.elm
+echo "Generating vocabulary with vocabulary..."
+npx ajv validate --errors text -s script/schema/vocabulary.schema.json -d script/data/vocabulary.yaml
+npx baldrick-whisker@latest render script/data/vocabulary.yaml script/template/vocabulary.hbs src/Bubblegum/TextArea/Vocabulary.elm
